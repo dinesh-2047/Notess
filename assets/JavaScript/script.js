8f3af1ba903a2f5d0a529ccfe4b74992bf3fb097
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-01-30 22:08:00 +0530

    js file updated

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,27 +1,19 @@
-// JavaScript to toggle between light and dark modes
-const toggleBtn = document.createElement("button");
-toggleBtn.classList.add("dark-mode-btn");
-toggleBtn.textContent = "🌙"; // Dark mode button icon
-
-document.body.appendChild(toggleBtn);
-
-// Check if user has previously selected dark mode
-if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark-mode");
-} else {
-    document.body.classList.add("light-mode");
-}
-
-toggleBtn.addEventListener("click", () => {
-    if (document.body.classList.contains("dark-mode")) {
-        document.body.classList.remove("dark-mode");
-        document.body.classList.add("light-mode");
-        toggleBtn.textContent = "🌙"; // Change button icon to dark mode
-        localStorage.setItem("theme", "light");
-    } else {
-        document.body.classList.remove("light-mode");
-        document.body.classList.add("dark-mode");
-        toggleBtn.textContent = "🌞"; // Change button icon to light mode
-        localStorage.setItem("theme", "dark");
-    }
-});
+      
+      //navbar
+      
+      // Get the elements
+      const menuBtn = document.getElementById('menu-btn');
+      const menu = document.querySelector('.menu');
+      
+      // Add event listener to toggle the menu on click of the button
+      menuBtn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+      });
+      
+      // Close the menu when clicking outside of it
+      document.addEventListener('click', (e) => {
+        if (!menu.contains(e.target) && !menuBtn.contains(e.target)) {
+          menu.classList.remove('active');
+        }
+      });
+      
